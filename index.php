0c1ffc177aaeb91305bb69bf4732a13397e43783
Johngtka <johngtka@gmail.com>
2023-08-19 14:13:34 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Methods: GET, POST");
+
+$config = require_once ('./dbConnect.php');
+
+try {
+
+    $db = new PDO("mysql:host={$config['host']};dbname={$config['db']};charset=utf8", $config['user'], $config['pass'], [PDO::ATTR_EMULATE_PREPARES => false, PDO::ERRMODE_EXCEPTION]);
+
+} catch (PDOException $error) {
+    echo $error->getCode();
+}
+
+?>
