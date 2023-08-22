9f7f8efa731dc96ff57e6c1678effc8a8d824fe1
Johngtka <johngtka@gmail.com>
2023-08-22 09:42:08 +0200

    feat: secured the double calling get method of request

--- api.php
+++ api.php
@@ -7,7 +7,17 @@
 
         if($_SERVER['REQUEST_METHOD'] === 'GET'){
             $users.getUsers();
-            $users.getUpdates();
+        }
+
+        if($_SERVER['REQUEST_METHOD']=== 'POST'){
+            
+            $requestData = json_decode(file_get_contents('php://input'), true);
+
+
+            if(isset($requestData['getUpdates']) && $requestData['getUpdates'] != false){
+                $updates.getUpdates();
+            }
+
         }
     
     }
