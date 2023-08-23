1e9501223d088200ad1ec2738660dba8ae683699
Johngtka <79154804+Johngtka@users.noreply.github.com>
2023-08-23 12:21:42 +0200

    feat: added updates module to api (#2)

--- api.php
+++ api.php
@@ -3,9 +3,21 @@
     function API(){
 
         $users = require_once('./routes/users.php');
+        $updates = require_once('./routes/updates.php');
 
         if($_SERVER['REQUEST_METHOD'] === 'GET'){
             $users.getUsers();
+        }
+
+        if($_SERVER['REQUEST_METHOD']=== 'POST'){
+            
+            $requestData = json_decode(file_get_contents('php://input'), true);
+
+
+            if(isset($requestData['getUpdates']) && $requestData['getUpdates'] != false){
+                $updates.getUpdates();
+            }
+
         }
     
     }
