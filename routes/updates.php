485bc75484809ddcc16f45f11ef5d2f8529f7d4e
Johngtka <79154804+Johngtka@users.noreply.github.com>
2023-09-05 12:26:01 +0200

    fix: repaired the methods in products and updates modules (#6)

--- routes/updates.php
+++ routes/updates.php
@@ -15,16 +15,20 @@ function postUpdate($newUpdate){
     $query -> bindValue(":date", $newUpdate['date'], PDO::PARAM_STR);
     $query -> bindValue(":desc", $newUpdate['desc'], PDO::PARAM_STR);
     $query -> execute();
+    $result = $query->fetchAll();
+    echo json_encode($result);
 }
 
 function editUpdate($editedLog){
     global $db;
-    $query = $db -> prepare("UPDATE aktualizacje SET Nazwa = :name, Data = :date, Opis = :desc WHERE id = :updateID");
+    $query = $db -> prepare("UPDATE aktualizacje SET name = :name, date = :date, description = :desc WHERE id = :updateID");
     $query -> bindValue(":name", $editedLog['name'], PDO::PARAM_STR);
     $query -> bindValue(":date", $editedLog['date'], PDO::PARAM_STR);
     $query -> bindValue(":desc", $editedLog['desc'], PDO::PARAM_STR);
     $query -> bindValue(":updateID", $editedLog['id'], PDO::PARAM_INT);
     $query -> execute();
+    $result = $query->fetchAll();
+    echo json_encode($result);
 }
 
 ?>
