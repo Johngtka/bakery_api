756c13dec424ead7133a44c027674bc20ab7f38d
Johngtka <79154804+Johngtka@users.noreply.github.com>
2024-06-21 10:08:14 +0200

    feat: veryfing discount code from user/order by sales discount code (#15)

--- routes/users.php
+++ routes/users.php
@@ -44,3 +44,27 @@ function getUsersOrders($userLogin)
         echo json_encode([]);
     }
 }
+
+function userOrderDiscountCodeChecker($orderDiscountCode)
+{
+    global $db;
+    $query = $db->prepare("SELECT zamowienia.SaleCode AS orderCode, promocje.SaleCode as discountCode FROM zamowienia JOIN promocje ON zamowienia.SaleCode = promocje.SaleCode WHERE zamowienia.SaleCode = :discountCode");
+    $query->bindValue(':discountCode', $orderDiscountCode, PDO::PARAM_STR);
+    $query->execute();
+    $result = $query->fetchAll();
+
+    if (!empty($result)) {
+        foreach ($result as $row) {
+            foreach ($row as $key => $value) {
+                if (is_numeric($key)) {
+                    continue;
+                }
+                $filteredRow[$key] = $value;
+            }
+            $filteredResult[] = $filteredRow;
+        }
+        echo json_encode($filteredResult);
+    } else {
+        echo json_encode([]);
+    }
+}
